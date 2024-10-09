5477e3fc6ac6b89f0d0395ae325090cfdab7e213
Salgado3 <jsalg10@gmail.com>
2024-10-09 00:57:25 -0500

    Add github Actions workflow for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "salgado3.github.io",
+  site: "https://www.jaimecodes.dev/",
 });
